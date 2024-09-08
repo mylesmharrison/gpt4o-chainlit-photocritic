282a7d6ba825afbaf689cc59b11177a4f73c3f33
Myles Harrison <mylesmharrison@gmail.com>
2024-09-08 10:30:42 -0400

    rewrite - text chat working

--- photocritic.py
+++ photocritic.py
@@ -1,11 +1,8 @@
 import os
-from base64 import b64encode
-from openai import AsyncOpenAI
 import chainlit as cl
+from openai import OpenAI, AsyncOpenAI
+from base64 import b64encode
 
-
-# OpenAI setup
-cl.instrument_openai()
 token = os.environ["GITHUB_TOKEN"]
 settings = {
     "model": "gpt-4o",
@@ -14,14 +11,11 @@ settings = {
 }
 
 client = AsyncOpenAI(base_url="https://models.inference.ai.azure.com", api_key=token)
-#client= AsyncOpenAI()
 
 # Helper function from https://github.com/GianfrancoCorrea/gpt-4-vision-chat/
-def process_image(image):
+def process_image(imagepath):
     # Accessing the bytes of a specific image
     
-    imagepath = image[0].path
-
     with open(imagepath, "rb") as image_file:
         image_base64 = b64encode(image_file.read()).decode('utf-8')
 
@@ -31,84 +25,56 @@ def process_image(image):
         
     return image_base64
 
-@cl.on_message
-
-async def on_message(msg: cl.Message):
-
-    if not msg.elements:
-        stream = await client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are a digital photo critic. You will provide helpful feedback on photographs on how to improve them."
-                },
-                {
-                    "role": "user",
-                    "content": msg.content
-                }
-            ],
-            stream=True,
-            **settings
-        )
-    else:
-        # Processing images exclusively
-        images = [file for file in msg.elements if "image" in file.mime]
-        base64_image = process_image(images)
-        print(msg.content)
-        # Generate response
-        stream = await client.chat.completions.create(
-            messages=[
-                {
-                    "role": "system",
-                    "content": "You are a digital photo critic. You will provide helpful feedback on photographs on how to improve them.",
-                    
-                },
-                {
-                    "role": "user",
-                    "content": [
-                        {
-                            "type": "text",
-                            "text": msg.content
-                        },
-                        {
-                             "type": "image_url",
-                             #"image_url": {"url": f"data:image/jpeg;base64,{base64_image}"}
-                             "image_url": {"url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",},
-                        }
-                    ]                    
-                }
-            ],
-            stream=True,
-            **settings
-        )
-
-
-    new_message = cl.Message(content="")
-
-    async for part in stream:
-        if token := part.choices[0].delta.content or "":
-            await new_message.stream_token(token)
-
-    await new_message.update()
-
-
-    #await cl.Message(content=response.choices[0].message.content).send()
-
-
 @cl.on_chat_start
 async def start():
 
-    # Attach the image to the message
-    await cl.Message(
-        content="Hello! I'm the GPT-4o Photo Critic! How can I help?",
-    ).send()
+    cl.user_session.set(
+        "message_history",
+        [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant."
+            },
+            {
+                "role":"assistant",
+                "content":"Hello and welcome!"
+            }
 
+        ],
+    )
 
-'''
-async def main(msg: cl.Message):
+    new_message = cl.Message(content="Hello and welcome")
+    await new_message.send()
+
+@cl.on_message
+async def onmessage(msg: cl.Message):
+
+    # Get the message history
     message_history = cl.user_session.get("message_history")
-    message_history.append({"role": "user", "content": message.content})
 
-    msg = cl.Message(content="")
-    await msg.send()
-'''
+    # Check if the chat message has an image attached
+    
+    if msg.elements:
+        climage = msg.elements[0]
+        #climage = cl.Image(path="./sample.jpg", name="image1", display="inline")
+        base64_image = process_image(climage.path)
+    
+    # Append to appropriate history
+    message_history.append({"role": "user", "content": msg.content})
+
+    # Set up response
+    response_msg = cl.Message(content="") 
+    stream = None\
+
+    stream = await client.chat.completions.create(
+        model="gpt-4o-mini",
+        messages=message_history,    
+        stream=True
+    )
+    
+    message_history.append({"role": "system", "content": response_msg.content})
+    
+    async for part in stream:
+        if token := part.choices[0].delta.content:
+            await response_msg.stream_token(token)
+    await response_msg.update()
